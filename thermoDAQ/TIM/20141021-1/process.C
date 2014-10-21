59c662249e7b3c743a44e1a71f6c71e1582783b6
Negusbuk <andreas.mussgiller@desy.de>
2014-10-21 10:54:57 +0200

    added new analysis files

---
+++ thermoDAQ/TIM/20141021-1/process.C
@@ -0,0 +1,17 @@
+#define process_cxx
+
+#include <TFile.h>
+#include <TTree.h>
+
+void process()
+{
+  TFile *file = TFile::Open("20141021-1.root");
+
+  TTree *T = (TTree*)file->Get("thermoDAQ");
+  
+  //T->Process("TempVsTime.C+");
+  //T->Process("Calibration.C+", "5500 5700");
+  //T->Process("CalTempVsTime.C+");
+  
+  T->Process("Analysis.C+", "15 7 0 11400 11600");
+}
