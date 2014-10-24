2ceb25a7e3da2846261bfea179ecbe62fb5a87c5
Negusbuk <Andreas Mussgiller>
2014-10-24 17:37:27 +0200

    update of ranges

--- thermoDAQ/TIM/20140821-1/process.C
+++ thermoDAQ/TIM/20140821-1/process.C
@@ -10,8 +10,10 @@ void process()
   TTree *T = (TTree*)file->Get("thermoDAQ");
   
   //T->Process("TempVsTime.C+");
-  //T->Process("Calibration.C+", "27000 28000");
+  //T->Process("Calibration.C+", "5700 5900");
   //T->Process("CalTempVsTime.C+");
   
-  T->Process("Analysis.C+", "15 7 0 21600 22000");
+  //T->Process("Analysis.C+", "15 7 0 13600 13800");
+  //T->Process("Analysis.C+", "15 7 0 17700 17900");
+  T->Process("Analysis.C+", "15 7 0 21800 22000");
 }
