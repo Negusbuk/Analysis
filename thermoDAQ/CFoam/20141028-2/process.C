788f670923404365ebe0c127d538e547e484d9c9
Negusbuk <Andreas Mussgiller>
2014-10-29 09:52:04 +0100

    added new analysis files

---
+++ thermoDAQ/CFoam/20141028-2/process.C
@@ -0,0 +1,17 @@
+#define process_cxx
+
+#include <TFile.h>
+#include <TTree.h>
+
+void process()
+{
+  TFile *file = TFile::Open("20141028-2.root");
+
+  TTree *T = (TTree*)file->Get("thermoDAQ");
+  
+  //T->Process("TempVsTime.C+");
+  //T->Process("Calibration.C+", "5150 5350");
+  //T->Process("CalTempVsTime.C+");
+  
+  T->Process("Analysis.C+", "15 7 0 11050 11250");
+}
