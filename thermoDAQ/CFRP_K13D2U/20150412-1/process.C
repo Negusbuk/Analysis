c50e3cca6f6b20ca637c1f7a9d2a756ca03224c8
Negusbuk <Andreas Mussgiller>
2015-04-13 12:41:38 +0200

    added processing macro

---
+++ thermoDAQ/CFRP_K13D2U/20150412-1/process.C
@@ -0,0 +1,19 @@
+#define process_cxx
+
+#include <TFile.h>
+#include <TTree.h>
+
+void process()
+{
+  TFile *file = TFile::Open("20150412-1.root");
+
+  TTree *T = (TTree*)file->Get("thermoDAQ");
+  
+  //T->Process("TempVsTime.C+");
+  //T->Process("Calibration.C+", "20000 20200");
+  //T->Process("CalTempVsTime.C+");
+
+  //T->Process("Analysis.C+", "31 0 31700 32200");
+  //T->Process("Analysis.C+", "31 0 45900 46400");
+  T->Process("Analysis.C+", "31 0 59300 59800");
+}
